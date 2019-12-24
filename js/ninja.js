d37f01eef768d83b85c75708ce40bd1323f86f5b
rjatkumarpensia <rjatkumarpensia@gmail.com>
2019-12-24 10:51:21 +0530

    First Commit

---
+++ js/ninja.js
@@ -0,0 +1,15 @@
+
+
+$(document).on('ready', function(){
+    
+});
+$(".quiz-slider").slick({
+    dots: true,
+    arrows:false,
+    infinite: true,
+    speed: 1000,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed : 5000,
+  });
